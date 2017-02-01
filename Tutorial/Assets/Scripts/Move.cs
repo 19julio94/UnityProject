9aec681cf8d6ccce52ea8b55edc014393b0eaef8
19julio94 <juliopatriciodasilva@gmail.com>
2017-02-01 16:53:25 +0100

    Movimientos con teclas,fisicas
    Hemos creado otro script para trabajar con las fuerzas importando la
    masa y sin importar la masa,ademas de los movimientos del cubo con las
    teclas

--- Tutorial/Assets/Scripts/Move.cs
+++ Tutorial/Assets/Scripts/Move.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-	float deltaRotation =30f;
-	float deltaMovement=10f;
+	public float deltaRotation =30f;
+	public float deltaMovement=10f;
+	public Color color;
+
+	//public float r,g,b; Variable float para elegir el color que nosotros queramos
+
 
 	// Use this for initialization
 	void Start () 
 	{
-		 
+		//Con este metodo lo que conseguimos es que nos salga un selector de color ya asi colorear nuestro cubo con el color seleccionado
+		GetComponent<Renderer> ().material.color = color;
+		//GetComponent<Renderer> ().material.color = new Color (r, g, b);
+		//Con este metodo podremos elegir el color de nuestro cubo ,no es muy exacto ya que hasat que le demos a play no sabremos que color hemos elegido
 	}
 	
 	// Update is called once per frame
---
+++ Tutorial/Assets/Scripts/Physics.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Physics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//velocidad instantanea usando la masa
+		GetComponent<Rigidbody>().AddForce(Vector3.right * 50f,ForceMode.Impulse);
+
+		//velocidad instantanea ignorando la masa
+		//GetComponent<Rigidbody>().AddForce(Vector3.right * 50f,ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
