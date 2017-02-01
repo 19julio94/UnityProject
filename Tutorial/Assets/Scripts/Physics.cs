85958023f4a5668b522e8a7851e486380503ca40
19julio94 <juliopatriciodasilva@gmail.com>
2017-02-01 17:19:44 +0100

    Ejercicio con los cuatro tipos de fuerzas,con y sin importar la masa

--- Tutorial/Assets/Scripts/Physics.cs
+++ Tutorial/Assets/Scripts/Physics.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class Physics : MonoBehaviour {
 
+	public Rigidbody rb;
+
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		//velocidad instantanea usando la masa
-		GetComponent<Rigidbody>().AddForce(Vector3.right * 50f,ForceMode.Impulse);
+		//GetComponent<Rigidbody>().AddForce(Vector3.right * 50f,ForceMode.Impulse);
 
 		//velocidad instantanea ignorando la masa
 		//GetComponent<Rigidbody>().AddForce(Vector3.right * 50f,ForceMode.VelocityChange);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		//Fuerzas contuinuas
+		//Fuerza continua dependiendo de la masa
+
+		//rb = GetComponent<Rigidbody>(); 
+		//rb.AddForce (new Vector3(1f, 0f, 1f) * 50f, ForceMode.Force);
+		//Fuerza continua independiente de la masa
+
+		//GetComponent<Rigidbody>().AddForce(Vector3.right * 50f,ForceMode.Acceleration);
+
+		//Fuerza que cancela la gravedad
+
+		rb = GetComponent<Rigidbody>(); 
+		rb.AddForce (Vector3.up * 50f, ForceMode.Force);
 		
 	}
 }
